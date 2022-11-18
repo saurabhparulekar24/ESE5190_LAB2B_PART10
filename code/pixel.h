4602a96cbf305787c464cbd3cfeb12424b0fd364
Saurabh Parulekar <57740824+saurabhparulekar24@users.noreply.github.com>
2022-11-17 19:55:30 -0500

    Add files via upload

---
+++ code/pixel.h
@@ -0,0 +1,7 @@
+#ifndef PIXEL_H    //include guards to avoid double inclusion of library   
+#define PIXEL_H    //include guards to avoid double inclusion of library
+
+void pixel_init();
+void pixel_set(uint32_t rgb);
+
+#endif
